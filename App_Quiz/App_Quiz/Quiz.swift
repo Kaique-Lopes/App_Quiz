376f853fd9f326f44291bc3ecc78496440237293
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-04-21 22:24:49 -0300

    validateOption

--- App_Quiz/App_Quiz/Quiz.swift
+++ App_Quiz/App_Quiz/Quiz.swift
@@ -8,5 +8,18 @@
 import Foundation
 
 class Quiz {
+    let question: String
+    let options: [String]
+    let correctedAnswer: String
+    
+    init(question: String, options: [String], correctedAnswer: String) {
+        self.question = question
+        self.options = options
+        self.correctedAnswer = correctedAnswer
+    }
+    func validateOption(_ index: Int) -> Bool{
+            let answer = options[index]
+        return answer == correctedAnswer
+    }
     
 }
